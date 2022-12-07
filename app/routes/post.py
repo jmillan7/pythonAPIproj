21a298a9f6ab3005e831acfed6cf5ab8976db61a
Juan Millan <jmillan@catalyte.io>
2022-12-07 15:31:12 -0600

    initial commit, working skeleton

---
+++ app/routes/post.py
@@ -0,0 +1,83 @@
+from typing import List, Optional
+from .. import models, schemas, utils, oauth2
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from ..database import get_db
+
+router = APIRouter(
+    prefix="/posts",
+    tags=['Posts']
+)
+@router.get("/", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db), limit:int = 5, skip: int = 0, search: Optional[str] = ""):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
+    return posts
+
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_current_user)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
+    new_post = models.Post(owner_id=curr_user.id, **post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
+    return new_post
+
+
+@router.get('/{id}')
+def get_post(id: int, db: Session=Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail= f"post with id: {id} can not be found")
+    return post
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session=Depends(get_db), curr_user: int = Depends(oauth2.get_current_user)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    
+    if post.first().owner_id != curr_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized to perform requested action")
+
+    post.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put('/{id}', response_model=schemas.PostCreate)
+def update_post(id: int, post: schemas.PostCreate, db: Session=Depends(get_db), curr_user: int = Depends(oauth2.get_current_user)):
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, id))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    updated_post = db.query(models.Post).filter(models.Post.id == id)
+    u_post = updated_post.first()
+
+    if u_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    
+    if u_post.owner_id != curr_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized to perform requested action")
+    
+    updated_post.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return post
+
